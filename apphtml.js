a85feb855f3b6b00d910e0f44442ccf2096f50b3
hondamarlboro <hondamarlboromclaren@gmail.com>
2013-10-10 07:43:55 +0900

    Update apphtml.js

--- apphtml.js
+++ apphtml.js
@@ -82,9 +82,10 @@
     function getWebApi() {
         var s = d.createElement("script"),
             src = "";
-        if (appId != "") src = "http://itunes.apple.com/jp/lookup?id=" + appId + "&lang=ja_jp&country=JP&entity=" + knd + "&limit=" + cnt;
-        //検索対象がMac Appの場合にSearch APIの不具合を回避する暫定措置としてattributeパラメーターを追加（ただし開発者名での検索不可）
-        if (src == "" && knd == "macSoftware") {
+        if (appId != "") {
+            src = "http://itunes.apple.com/jp/lookup?id=" + appId + "&lang=ja_jp&country=JP&entity=" + knd + "&limit=" + cnt;
+        } //検索対象がMac Appの場合にSearch APIの不具合を回避する暫定措置としてattributeパラメーターを追加（ただし開発者名での検索不可）
+        else if (src == "" && knd == "macSoftware") {
             src = "http://itunes.apple.com/jp/search?term=" + encodeURIComponent(kwd) + "&lang=ja_jp&country=JP&entity=" + knd + "&limit=" + cnt + "&attribute=allTrackTerm";
         } else {
             src = "http://itunes.apple.com/jp/search?term=" + encodeURIComponent(kwd) + "&lang=ja_jp&country=JP&entity=" + knd + "&limit=" + cnt;
